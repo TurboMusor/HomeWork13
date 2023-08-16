84b9c2d4a0037fc8d5c7b041215467f1cf9a5f98
TurboMusor <shnipov770@gmail.com>
2023-08-16 22:39:15 +0300

    -must work

---
+++ Src/Main.java
@@ -0,0 +1,22 @@
+class Person {
+    public String personInfo(String firstName, String lastName, String city, String phoneNumber) {
+        return "Зателефонувати громадянинові " + firstName + " " + lastName + " з міста " + city + " можна за номером " + phoneNumber + ".";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        String info1 = person.personInfo("Will", "Smith", "New York", "2936729462846");
+        System.out.println(info1);
+
+        String info2 = person.personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        System.out.println(info2);
+
+        String info3 = person.personInfo("Sherlock", "Holmes", "London", "37742123513");
+        System.out.println(info3);
+    }
+}
+
+
