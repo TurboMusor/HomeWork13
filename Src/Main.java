71a2fda12ce768a0985d25612911723158c59b3f
TurboMusor <shnipov770@gmail.com>
2023-08-17 22:20:21 +0300

    -today work

--- Src/Main.java
+++ Src/Main.java
@@ -1,8 +1,3 @@
-class Person {
-    public String personInfo(String firstName, String lastName, String city, String phoneNumber) {
-        return "Зателефонувати громадянинові " + firstName + " " + lastName + " з міста " + city + " можна за номером " + phoneNumber + ".";
-    }
-}
 
 public class Main {
     public static void main(String[] args) {
---
+++ Src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public String personInfo(String firstName, String lastName, String city, String phoneNumber) {
+        return "Зателефонувати громадянинові " + firstName + " " + lastName + " з міста " + city + " можна за номером " + phoneNumber + ".";
+    }
+}
+
+
+
+
